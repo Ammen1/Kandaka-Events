483b2adee992c207e282ec9951f6f28535a14508
Tamirat <amenguda@gmail.com>
2024-07-30 04:35:43 +0300

    Kandaka Events

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 import HowTo from "./HowTo";
 import Business from "./Business";
 import styles from "@/style";
-import Products from "./products";
+import Products from "./Products";
 import Sub from "./Sub";
 import Team from "./Team";
 import Footer from "./Footer";
